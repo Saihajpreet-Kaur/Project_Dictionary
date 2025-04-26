693eccd2222fbe01d65cb8f103eb9a7a4bcf9535
ronitbby <ronit0699.becse24@chitkara.edu.in>
2025-04-26 23:32:58 +0530

    Update history display  script.js

--- script.js
+++ script.js
@@ -274,7 +274,26 @@ function addToHistory(word) {
   // Update display
   updateHistoryDisplay();
 }
-
+function updateHistoryDisplay() {
+  historyList.innerHTML = '';
+  
+  if (searchHistory.length > 0) {
+    emptyHistory.style.display = 'none';
+    
+    searchHistory.forEach((historyWord) => {
+      const li = document.createElement('li');
+      li.innerHTML = `
+        <button onclick="fetchWordData('${historyWord}')">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-history"><path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"/><path d="M3 3v5h5"/><path d="M12 7v5l4 2"/></svg>
+          ${historyWord}
+        </button>
+      `;
+      historyList.appendChild(li);
+    });
+  } else {
+    emptyHistory.style.display = 'block';
+  }
+}
 
 
 
